af04dae0b53b125cf2ca594b2a3f07c743a535b5
Allen Garvey <allen.garvey@gmail.com>
2017-01-08 13:59:31 -0500

    Edit routing so that admin and public sites use different layouts

--- web/router.ex
+++ web/router.ex
@@ -13,9 +13,15 @@ defmodule Artour.Router do
     plug :accepts, ["json"]
   end
 
+  # based on:
+  # http://www.cultivatehq.com/posts/how-to-set-different-layouts-in-phoenix/
+  pipeline :public_layout do
+    plug :put_layout, {Artour.LayoutView, :public}
+  end
+
   # public site
   scope "/", Artour do
-    pipe_through :browser # Use the default browser stack
+    pipe_through [:browser, :public_layout] # Use the default browser stack
 
     get "/", PageController, :index
 
