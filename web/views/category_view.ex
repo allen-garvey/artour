4c7f829dfc365cf6d8e8604eceac3a2ebe138966
Allen Garvey <allen.garvey@gmail.com>
2017-01-08 23:55:21 -0500

    Add documentation to functions in category view

--- web/views/category_view.ex
+++ web/views/category_view.ex
@@ -1,10 +1,18 @@
 defmodule Artour.CategoryView do
   use Artour.Web, :view
 
+  @doc """
+  Used on index page - transforms category type integer into title-case
+  string
+  """
   def category_type_name(category_type_value) do
     Artour.CategoryType.name(category_type_value) |> String.capitalize
   end
 
+  @doc """
+  Used on index page - takes category index and returns list of 
+  formatted values
+  """
   def index_row_values(category) do
   	[category.name, category.slug, category_type_name(category.type)]
   end
