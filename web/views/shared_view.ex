d3da6a7e41e5cd90e48d8aabfcf25049776a198c
Allen Garvey <allen.garvey@gmail.com>
2017-01-10 13:07:19 -0500

    Add functions to shared view to generate new and edit form page headings

--- web/views/shared_view.ex
+++ web/views/shared_view.ex
@@ -35,4 +35,17 @@ defmodule Artour.SharedView do
 		apply(Artour.Router.Helpers, item_path_func_name(item_name_singular), [conn, path_atom, item_instance])
 	end
 
+	@doc """
+  	Used as heading for new  and edit item form
+  	for :new, item_name should be item_name_singular
+  	for :edit, item_name should be item display_name
+  	"""
+  	def form_heading(item_name, form_type) do
+  		if form_type == :edit do
+  			"Edit " <> item_name
+  		else
+  			"New " <> item_name
+  		end
+  	end
+
 end
