d73087baeeedd2357b04006c916d9964c6e84e38
Allen Garvey <allen.garvey@gmail.com>
2017-01-10 13:22:06 -0500

    Add checks for string length to slug validation

--- web/model_helpers/slug_validator.ex
+++ web/model_helpers/slug_validator.ex
@@ -7,10 +7,13 @@ defmodule Artour.SlugValidator do
 	Slug must only contain lowercase letters, numbers and hyphens
 	slug cannot begin or end with hyphen and repeating hyphens
 	are not allowed
+	Slug cannot be empty and cannot be more than 80 characters
 	"""
 	def is_valid_slug(slug) do
 		cond do
 			is_nil(slug) -> false
+			String.length(slug) == 0 -> false
+			String.length(slug) > 80 -> false
 			true -> Regex.match?(~r/^[a-z0-9]*(-[a-z0-9]|[a-z0-9])*$/, slug)
 		end
 	end
@@ -24,7 +27,7 @@ defmodule Artour.SlugValidator do
 		if is_valid_slug(slug_value) do
 			changeset
 		else
-			add_error(changeset, slug_key, "Only lowercase letters, numbers and hyphens are allowed. Cannot begin or end with a hyphen and hyphens cannot repeat")
+			add_error(changeset, slug_key, "Only lowercase letters, numbers and hyphens are allowed. Cannot begin or end with a hyphen, hyphens cannot repeat and must be less than 80 characters long")
 		end
 	end
 end
