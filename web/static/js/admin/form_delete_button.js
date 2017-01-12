1edea15e109adee3ef515b79b9b1a5822396b8a0
Allen Garvey <allen.garvey@gmail.com>
2017-01-12 15:21:06 -0500

    Add javascript to enable delete functionality on edit form pages

---
+++ web/static/js/admin/form_delete_button.js
@@ -0,0 +1,17 @@
+/*
+ * Used in edit form to delete items
+ */
+(function($){
+	$("[data-button='delete']").on('click', function(event){
+		event.preventDefault();
+		var deleteForm = document.querySelector("[data-form='delete']");
+		if(!deleteForm){
+			return;
+		}
+		if(!window.confirm("Are you sure you want to delete this item?")){
+			return;
+		}
+		deleteForm.submit();
+	});
+ 
+})(aQuery);
