1b8858d0420989a8fc60a2da614c64c89ec037d3
Allen Garvey <allen.garvey@gmail.com>
2017-01-13 13:28:32 -0500

    Add functions to layout helpers to format datetime strings

--- web/views/layout_helpers.ex
+++ web/views/layout_helpers.ex
@@ -6,4 +6,30 @@ defmodule Artour.LayoutHelpers do
 	def site_title() do
 		"Strange Scenery"	
 	end
+
+	@doc """
+  	Takes datetime string as argument
+  	returns tuple of integers
+  	{YYYY, MM, DD}
+  	"""
+	def datetime_to_tuple(datetime) do
+		datetime
+			|> Ecto.DateTime.cast! 
+			|> Ecto.DateTime.to_date
+			|> Ecto.Date.dump
+			|> elem(1)
+	end
+
+	@doc """
+  	Takes datetime string as argument
+  	returns string date in format MM-DD-YYYY
+  	"""
+	def datetime_to_us_date(datetime) do
+		date_tuple = datetime_to_tuple(datetime)
+		Integer.to_string(elem(date_tuple, 1)) <> 
+		"-" <>
+		Integer.to_string(elem(date_tuple, 2)) <> 
+		"-" <>
+		Integer.to_string(elem(date_tuple, 0))
+	end
 end
