ac9154efb52f4658ce10867f33269386763ebe58
Allen Garvey <allen.garvey@gmail.com>
2017-01-12 22:14:45 -0500

    Refactor delete button javascript to combine return statements

--- web/static/js/admin/form_delete_button.js
+++ web/static/js/admin/form_delete_button.js
@@ -5,10 +5,7 @@
 	$("[data-button='delete']").on('click', function(event){
 		event.preventDefault();
 		var deleteForm = document.querySelector("[data-form='delete']");
-		if(!deleteForm){
-			return;
-		}
-		if(!window.confirm("Are you sure you want to delete this item?")){
+		if(!deleteForm || !window.confirm("Are you sure you want to delete this item?")){
 			return;
 		}
 		deleteForm.submit();
