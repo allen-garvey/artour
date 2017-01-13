da68b84a6bffe19ff7fb63d7b010ef1509782beb
Allen Garvey <allen.garvey@gmail.com>
2017-01-13 14:04:59 -0500

    Add function to convert a string to paragraphs using newlines

--- web/views/layout_helpers.ex
+++ web/views/layout_helpers.ex
@@ -1,5 +1,7 @@
 # For code shared across views that relates to whole application
 defmodule Artour.LayoutHelpers do
+	use Phoenix.HTML
+
 	@doc """
   	Used in title of site, in such places as the title tag
   	"""
@@ -32,4 +34,20 @@ defmodule Artour.LayoutHelpers do
 		"-" <>
 		Integer.to_string(elem(date_tuple, 0))
 	end
+
+	@doc """
+  	Takes string and splits by newlines, converts into
+  	paragraph tags and returns string of combined paragraphs
+  	"""
+  	def to_paragraphs(text) do
+  		text
+  			|> String.split("\n")
+  			# remove empty strings to allow multiple line-breaks in raw text
+  			|> Enum.filter(&(String.length(&1) > 1))
+  			|> Enum.map(&(content_tag(:p, &1)))
+  			|> Enum.map(&safe_to_string/1)
+  			|> Enum.join
+  			|> raw
+  	end
+
 end
