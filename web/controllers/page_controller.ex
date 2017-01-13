5f0957a919a5d792e365d2b5307e718c659d6689
Allen Garvey <allen.garvey@gmail.com>
2017-01-13 15:07:21 -0500

    Edit public homepage to show list of posts

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule Artour.PageController do
   use Artour.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+  	posts = Repo.all(from(Artour.Post, order_by: [desc: :id])) |> Repo.preload([:category])
+    render conn, "index.html", posts: posts
   end
 end
