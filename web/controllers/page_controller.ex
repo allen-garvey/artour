750e576d93e1bc7c9d3a962cc175b12a7014346d
Allen Garvey <allen.garvey@gmail.com>
2017-01-14 16:00:10 -0500

    Edit browse page to only list categories that have at least 1 related post

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -6,8 +6,13 @@ defmodule Artour.PageController do
     render conn, "index.html", posts: posts
   end
 
+  @doc """
+  Shows list of categories that contain 1 or more related posts
+  """
   def browse(conn, _params) do
-  	categories = Repo.all(Artour.Category.default_order_query())
+  	category_id_subquery = Repo.all(from(p in Artour.Post, distinct: true, select: p.category_id))
+  	categories = Repo.all(from c in Artour.Category, where: c.id in ^category_id_subquery)
+    
     render conn, "browse.html", categories: categories
   end
 end
