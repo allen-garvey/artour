9236ef7ced1a36ea76c6e7c2cc5684e15c25b68e
Allen Garvey <allen.garvey@gmail.com>
2017-01-16 15:09:53 -0500

    Add functions to convert date string into US formatted date

--- web/views/layout_helpers.ex
+++ web/views/layout_helpers.ex
@@ -45,6 +45,18 @@ defmodule Artour.LayoutHelpers do
 	end
 
 	@doc """
+  	Takes date string as argument
+  	returns tuple of integers
+  	{YYYY, MM, DD}
+  	"""
+	def date_to_tuple(date) do
+		date
+			|> Ecto.Date.cast! 
+			|> Ecto.Date.dump
+			|> elem(1)
+	end
+
+	@doc """
   	Takes datetime string as argument
   	returns string date in format `Month name DD, YYYY`
   	"""
@@ -62,13 +74,29 @@ defmodule Artour.LayoutHelpers do
   	returns string date in format MM-DD-YYYY
   	"""
 	def datetime_to_us_date(datetime) do
-		date_tuple = datetime_to_tuple(datetime)
+		datetime |> datetime_to_tuple |> date_tuple_to_us_date
+	end
+
+	@doc """
+  	Takes date string as argument
+  	returns string date in format MM-DD-YYYY
+  	"""
+	def date_to_us_date(date) do
+		date |> date_to_tuple |> date_tuple_to_us_date
+	end
+
+	@doc """
+  	Takes date tuple in format {YYYY, MM, DD}
+  	returns string date in format MM-DD-YYYY
+  	"""
+	def date_tuple_to_us_date(date_tuple) do
 		Integer.to_string(elem(date_tuple, 1)) <> 
 		"-" <>
 		Integer.to_string(elem(date_tuple, 2)) <> 
 		"-" <>
 		Integer.to_string(elem(date_tuple, 0))
 	end
+
 
 	@doc """
   	Takes string and splits by newlines, converts into
