78b1e792fce12abecaef54f99b257b7466eb3bb9
Allen Garvey <allen.garvey@gmail.com>
2017-01-17 18:36:36 -0500

    Add functionality to image forms to autofill filenames based on image title

---
+++ web/static/js/admin/autofill_image_name.js
@@ -0,0 +1,51 @@
+/*
+ * Used to autofill image names on forms
+ * by using image title
+ */
+(function($){
+	var imageNameSource = $('[data-image-name]');
+	//sanity check first
+	if(imageNameSource.length < 1){
+		return;
+	}
+	//sets a textfield aQuery object sets value to 
+	//passed in value
+	//if it does not already have a value already
+	var autofillTextfield = function(textfield, value){
+		if(textfield.val() !== ''){
+			return;
+		}
+		textfield.val(value);
+	};
+	var FILE_SUFFIXES = ['lg', 'med', 'sm', 'thumb'];
+
+	var TEXTFIELD_TARGETS = ['large', 'medium', 'small', 'thumbnail'].map(function(elem){
+		return $('[data-image-' + elem + ']');
+	});
+
+	var suffixToImageFilename = function(basename, suffix){
+		return basename + '-' + suffix + '.jpg';
+	};
+
+	//converts string to lowercase and replaces all spaces and underscores with hyphens
+	var urlify = function(text){
+		if(!text){
+			return '';
+		}
+		return text.toLowerCase().replace(/&/g, 'and').replace(/@/g, 'at').replace(/[\.]+/g, ' ').replace(/[^a-z0-9-_\s]/g, '').replace(/[^\w\d]+/g, '-').replace(/^-+|-+$/g, '');
+	};
+	//when focus lost on image name source, autofill targets with pre-generated image name,
+	//unless they are already filled
+	imageNameSource.on('blur', function(event){
+		var imageName = urlify(this.value);
+		var fileNames = FILE_SUFFIXES.map(function(suffix){
+			return imageName + '-' + suffix + '.jpg';
+		});
+
+		TEXTFIELD_TARGETS.forEach(function(textfield, i){
+			autofillTextfield(textfield, suffixToImageFilename(imageName, FILE_SUFFIXES[i]));
+		});
+		
+	});
+
+})(aQuery);
