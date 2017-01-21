c6617f728cc0071a1b4845e7b652ecb32dd3f043
Allen Garvey <allen.garvey@gmail.com>
2017-01-21 15:34:24 -0500

    Refactor to_paragraphs layout helper to simplify code

--- web/views/layout_helpers.ex
+++ web/views/layout_helpers.ex
@@ -13,27 +13,16 @@ defmodule Artour.LayoutHelpers do
   	Takes string and splits by newlines, converts into
   	paragraph tags and returns string of combined paragraphs
   	"""
-  	def to_paragraphs(text) do
-  		cond do
-  			#is_binary tests if string
-  			is_binary(text) -> to_paragraphs_safe text
-  			true -> raw ""
-  		end
-  	end
+	def to_paragraphs(text) when is_binary(text) do
+    text
+      |> String.split("\n")
+      # remove empty strings to allow multiple line-breaks in raw text
+      |> Enum.filter(&(String.length(&1) > 1))
+      |> Enum.map(&(content_tag(:p, &1)))
+  end
 
-  	@doc """
-  	Same as to paragraphs, but text should be checked first that 
-  	argument is string and not nil
-  	"""
-  	def to_paragraphs_safe(text) do
-  		text
-  			|> String.split("\n")
-  			# remove empty strings to allow multiple line-breaks in raw text
-  			|> Enum.filter(&(String.length(&1) > 1))
-  			|> Enum.map(&(content_tag(:p, &1)))
-  			|> Enum.map(&safe_to_string/1)
-  			|> Enum.join
-  			|> raw
-  	end
+  def to_paragraphs(_not_string) do
+    raw ""
+  end
 
 end
