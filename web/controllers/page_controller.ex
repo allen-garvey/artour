e61ec9354fbd674d94d5917910b1a71c9a5a46b2
Allen Garvey <allen.garvey@gmail.com>
2017-01-21 23:41:06 -0500

    Edit homepage to use posts' cover images

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule Artour.PageController do
   use Artour.Web, :controller
 
   def index(conn, _params) do
-  	posts = Repo.all(from(Artour.Post, order_by: [desc: :id])) |> Repo.preload([:category])
+  	posts = Repo.all(from(Artour.Post, order_by: [desc: :id])) |> Repo.preload([:category, :cover_image])
     render conn, "index.html", posts: posts
   end
 
