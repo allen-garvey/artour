d83893e236d205b7daa157501b01e4d09938f29a
Allen Garvey <allen.garvey@gmail.com>
2017-01-27 16:29:54 -0500

    Add functionality to generate static pages for posts

--- lib/mix/artour/gen/html.ex
+++ lib/mix/artour/gen/html.ex
@@ -5,23 +5,90 @@ defmodule Mix.Tasks.Artour.Gen.Html do
 
   @shortdoc "Generates static html files from pages and saves in given directory"
   def run(_args) do
-    IO.puts "Generating HTML files" 
+    IO.puts "Generating HTML files"
+    #directory static files will be saved
+    dest_dir = File.cwd! |> Path.join("_build") |> Path.join("static_site")
+    #create dest dir if not exists
+    File.mkdir_p! dest_dir
+
     #start app so repo is available
     Mix.Task.run "app.start", []
     
-    #If want to do straight pipelines with no intermediate variables, must do
-    # |> Map.fetch(:resp_body)
-    # |> elem(1)
-    conn = conn(:get, "/") 
+    routes = page_routes() ++ post_routes()
+
+    for page_route <- routes do
+      conn = default_conn |> render_page_route(page_route)
+      IO.puts conn.resp_body
+    end
+    
+    
+    #Path.join(dest_dir, "index.html") |> File.write!(conn.resp_body, [:utf8, :append])
+  end
+
+  @doc """
+  Returns base conn struct
+  have to initialize with Phoenix.Controller.put_new_view before
+  Phoenix.Controller.render will work
+  """
+  def default_conn() do
+    conn(:get, "/") 
       |> Plug.Conn.put_private(:phoenix_endpoint, Artour.Endpoint)
       |> Phoenix.Controller.put_new_layout({Artour.LayoutView, "public.html"})
-      |> Phoenix.Controller.put_new_view(Artour.PostView)  
-      # |> Artour.PostController.index(%{})
-      #|> Map.fetch(:resp_body)
-      #|> elem(1)
-      #|> IO.puts
-    #have to do this to dynamically call controller
-    conn = apply(Artour.PostController, :index, [conn, %{}])
-    IO.puts conn.resp_body
+  end
+
+  @doc """
+  Returns conn after rendering page route
+  second argument should be list item from page_routes function
+  conn.resp_body will contain rendered page
+  """
+  def render_page_route(conn, {_path, controller, handler, params}) do
+    #can't use pipes if we want to dynamically call controller
+    conn = Phoenix.Controller.put_new_view(conn, default_view_for(controller))
+    apply(controller, handler, [conn, params])
+  end
+
+  @doc """
+  Returns default module name atom for a given controller
+  (the same as controller name with controller replaced by view)
+  """
+  def default_view_for(controller) when is_atom(controller) do
+    controller 
+      |> Atom.to_string 
+      |> String.replace_suffix("Controller", "View") 
+      |> String.to_atom
+  end
+
+  @doc """
+  Returns the filename to save a current path as
+  """
+  def filename_for(path) when is_binary(path) do
+    Path.join(path, "index.html")
+  end
+
+
+  @doc """
+  Returns list of tuples in format: path (string), controller module(atom), controller handler function (atom), params (map)
+  e.g. {"/posts", Artour.PostController, :index, %{}}
+  """
+  def page_routes() do
+    [
+      {"/", Artour.PageController, :index, %{}}, 
+      {"/browse", Artour.PageController, :browse, %{}}
+    ]
+  end
+
+  @doc """
+  Returns list of tuples in format: path (string), controller module(atom), controller handler function (atom), params (map)
+  e.g. {"/posts", Artour.PostController, :index, %{}}
+  """
+  def post_routes() do
+    Artour.Repo.all(Artour.Post) |> Enum.map(&post_to_route/1)
+  end
+
+  @doc """
+  Takes a post struct and returns route tuple
+  """
+  def post_to_route(post) do
+    {"/posts/" <> post.slug, Artour.PublicPostController, :show, %{"slug" => post.slug}}
   end
 end
