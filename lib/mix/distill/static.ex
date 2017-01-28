20555ce338fceda7c38e1a674bb724fa345cf49b
Allen Garvey <allen.garvey@gmail.com>
2017-01-28 14:01:15 -0500

    Create mix task to copy static assets to distilled site directory

---
+++ lib/mix/distill/static.ex
@@ -0,0 +1,48 @@
+#run task with 'mix distill.statc'
+defmodule Mix.Tasks.Distill.Static do
+  use Mix.Task
+
+  @shortdoc "Copies static assets to given directory"
+  def run(_args) do
+    dest_dir = Distill.Directory.default_dest_directory
+    #create root directory if it doesn't exist
+    File.mkdir_p! dest_dir
+
+    source_dir = default_static_assets_directory()
+
+    IO.puts "Copying static assets from " <> source_dir <> " to " <> dest_dir
+    IO.puts ""
+
+    for filename <- static_asset_filenames do
+      IO.puts "Copying " <> filename
+      dest_enclosing_dir = Path.join(dest_dir, Path.dirname(filename))
+      #make sure enclosing directory in dest_dir exists
+      File.mkdir_p! dest_enclosing_dir
+      File.cp Path.join(source_dir, filename), Path.join(dest_dir, filename)
+    end
+    
+  end
+
+  @doc """
+  Default directory for static assets
+  """
+  def default_static_assets_directory() do
+    File.cwd! 
+      |> Path.join("priv")
+      |> Path.join("static")
+  end
+
+  @doc """
+  Returns list of filenames of static assets
+  """
+  def static_asset_filenames() do
+    [
+      "favicon.ico",
+      "robots.text",
+      Path.join("css", "main.css")
+    ]
+  end
+
+  
+
+end
