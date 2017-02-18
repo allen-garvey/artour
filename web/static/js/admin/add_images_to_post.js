38718cdb75f8e06f56b9d368d0ce97962d20951a
Allen Garvey <allen.garvey@gmail.com>
2017-02-18 16:20:37 -0500

    Disable save button on add images to post page when no images are selected

--- web/static/js/admin/add_images_to_post.js
+++ web/static/js/admin/add_images_to_post.js
@@ -3,16 +3,42 @@
  * when checkbox is checked
  */
 (function($){
-	var checkboxSelector = '.post-add-images-list input[type="checkbox"]';
+	var checkboxes = $('.post-add-images-list input[type="checkbox"]');
+	//sanity check that we are on the right page
+	if(checkboxes.length === 0){
+		return;
+	}
+	var numCheckboxesChecked = 0;
     
-    $(checkboxSelector).on('change', function(event){
+    checkboxes.on('change', function(event){
     	$(this).closest('li').toggleClass('item-selected');
+    	if(this.checked){
+    		numCheckboxesChecked++;
+    	}
+    	else{
+    		numCheckboxesChecked--;
+    	}
+    	toggleSaveButton(numCheckboxesChecked);
     });
 
     //for when reloaded the page with checked checkboxes
-    $(checkboxSelector).each(function(index, el){
+    checkboxes.each(function(index, el){
     	if(el.checked){
     		$(el).closest('li').addClass('item-selected');
+    		numCheckboxesChecked++;
     	}
     });
+
+    //Disables save button if no checkboxes are checked
+    function toggleSaveButton(numCheckboxesChecked){
+    	var saveButton = document.querySelector('[data-role="save-images"]');
+    	if(numCheckboxesChecked === 0){
+    		saveButton.disabled = true;
+    	}
+    	else{
+    		saveButton.disabled = false;
+    	}
+    }
+    //toggle save button on startup
+    toggleSaveButton(numCheckboxesChecked);
 })(aQuery);
