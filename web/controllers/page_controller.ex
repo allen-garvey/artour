ffdfcbeeba56579e6fa37b7b554e2256e4a6fb8c
Allen Garvey <allen.garvey@gmail.com>
2017-03-09 21:46:58 -0500

    Implement basic index page pagination

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,9 +2,23 @@ defmodule Artour.PageController do
   use Artour.Web, :controller
   alias Artour.Page
 
+  def index(conn, %{"page_num" => page_num}) do
+    last_page = Page.last_page Repo
+    #no checking if page_num is valid or in range, because final product will be static site
+    page_num = cond do
+                  is_integer(page_num) -> page_num
+                  true -> String.to_integer page_num
+              end
+
+    posts = Repo.all(Page.posts_for_page_query(page_num)) |> Repo.preload([:category, :cover_image])
+    render conn, "index.html", posts: posts, current_page: page_num, last_page: last_page
+  end
+
+  @doc """
+  Index defaults to first page
+  """
   def index(conn, _params) do
-  	posts = Repo.all(from(Artour.Post, order_by: [desc: :id])) |> Repo.preload([:category, :cover_image])
-    render conn, "index.html", posts: posts
+    index conn, %{"page_num" => 1}
   end
 
   @doc """
--- web/models/page.ex
+++ web/models/page.ex
@@ -1,4 +1,36 @@
 defmodule Artour.Page do
+	import Ecto
+    import Ecto.Query
+
+	@doc """
+	Number of posts per page in index post list
+	"""
+	def posts_per_page() do
+		2
+	end
+
+	@doc """
+	Returns posts for current page
+	page_num is 1 indexed page
+	"""
+	def posts_for_page_query(page_num) when is_integer(page_num) do
+		post_offset = cond do
+		  				page_num <= 0 -> 1
+		  				true -> (page_num - 1) * posts_per_page  
+					end
+		
+		from(Artour.Post, order_by: [desc: :id], limit: ^posts_per_page, offset: ^post_offset)
+	end
+
+	@doc """
+	Returns last page number (1 indexed)
+	"""
+	def last_page(repo) do
+		post_count = repo.one!(from p in Artour.Post, select: count(p.id))
+		(1.0 * post_count / posts_per_page)
+			|> Float.ceil
+			|> round
+	end
   
 	@doc """
 	Body text for about page
