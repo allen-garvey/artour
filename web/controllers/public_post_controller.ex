c840d416b1bdc9017e8925645897cc8025c3f72c
Allen Garvey <allen.garvey@gmail.com>
2017-03-12 19:03:27 -0400

    Add title and image for facebook on public post show pages

--- web/controllers/public_post_controller.ex
+++ web/controllers/public_post_controller.ex
@@ -15,9 +15,9 @@ defmodule Artour.PublicPostController do
   Used on public site to show individual post
   """
   def show(conn, %{"slug" => slug}) do
-    post = Repo.get_by!(Post, slug: slug) |> Repo.preload([:category]) |> Repo.preload(tags: from(Artour.Tag, order_by: :name))
+    post = Repo.get_by!(Post, slug: slug) |> Repo.preload([:category, :cover_image]) |> Repo.preload(tags: from(Artour.Tag, order_by: :name))
     post_images = Post.album_post_images(Repo, post)
-    render conn, "show.html", page_title: Artour.PostView.display_name(post), post: post, post_images: post_images, javascript: true 
+    render conn, "show.html", page_title: Artour.PostView.display_name(post), post: post, post_images: post_images, javascript: true, facebook_image: post.cover_image 
   end
 
   
