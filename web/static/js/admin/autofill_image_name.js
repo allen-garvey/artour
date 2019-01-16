d1b62410d65285a69df0c55e055c9033231aa569
Allen Garvey <allen.garvey@gmail.com>
2019-01-16 11:29:34 -0500

    Change autofill image name script back to ES5 since current minify doesn't support it, and add button to regenerate image filenames

--- web/static/js/admin/autofill_image_name.js
+++ web/static/js/admin/autofill_image_name.js
@@ -2,28 +2,24 @@
  * Used to autofill image names on forms
  * by using image title
  */
-(function($){
-	const imageNameSource = $('[data-image-name]');
+(function(){
+	var imageNameSource = document.querySelector('[data-image-name]');
 	//sanity check first
-	if(imageNameSource.length < 1){
+	if(!imageNameSource){
 		return;
 	}
+	var regenerateButton = document.getElementById('autofill-image-button');
+	var isPortraitCheckbox = document.getElementById('is_portrait_checkbox');
+	var FILE_SUFFIXES = ['lg', 'med', 'sm', 'thumb'];
+	var FILE_SUFFIXES_PORTRAIT = ['med', 'med', 'sm', 'thumb'];
 
-	const isPortraitCheckbox = document.getElementById('is_portrait_checkbox');
-	const FILE_SUFFIXES = ['lg', 'med', 'sm', 'thumb'];
-	const FILE_SUFFIXES_PORTRAIT = ['med', 'med', 'sm', 'thumb'];
-
-	const TEXTFIELD_TARGETS = ['large', 'medium', 'small', 'thumbnail'].map(function(elem){
+	var TEXTFIELD_TARGETS = ['large', 'medium', 'small', 'thumbnail'].map(function(elem){
 		return document.querySelector('[data-image-' + elem + ']');
 	});
 	
-	//sets a textfield element value to 
-	//passed in value
-	//if it does not already have a value already
-	function autofillTextfield(textfield, value){
-		//val() is null if unset
-		const val = textfield.value;
-		if(val){
+	//sets a textfield element value to passed in value
+	function autofillTextfield(textfield, value, shouldForce){
+		if(!shouldForce && textfield.value){
 			return;
 		}
 		textfield.value = value;
@@ -40,24 +36,28 @@
 		return text.toLowerCase().replace(/&/g, 'and').replace(/@/g, 'at').replace(/[\.]+/g, ' ').replace(/[^a-z0-9-_\s]/g, '').replace(/[^\w\d]+/g, '-').replace(/^-+|-+$/g, '');
 	}
 
-	function autofillFields(inputText){
+	function autofillFields(inputText, shouldForce){
 		//don't do anything if input is blank
 		if(!inputText){
 			return;
 		}
-		const imageName = urlify(inputText);
-		const fileSuffixes = isPortraitCheckbox.checked ? FILE_SUFFIXES_PORTRAIT : FILE_SUFFIXES;
+		var imageName = urlify(inputText);
+		var fileSuffixes = isPortraitCheckbox.checked ? FILE_SUFFIXES_PORTRAIT : FILE_SUFFIXES;
 
 		TEXTFIELD_TARGETS.forEach(function(textfield, i){
-			autofillTextfield(textfield, suffixToImageFilename(imageName, fileSuffixes[i]));
+			autofillTextfield(textfield, suffixToImageFilename(imageName, fileSuffixes[i]), shouldForce);
 		});
 	}
 
 
 	//when focus lost on image name source, autofill targets with pre-generated image name,
 	//unless they are already filled
-	imageNameSource.on('blur', function(event){
-		autofillFields(this.value);
+	imageNameSource.addEventListener('blur', function(event){
+		autofillFields(this.value, false);
 	});
 
-})(aQuery);
+	regenerateButton.onclick = function(){
+		autofillFields(imageNameSource.value, true);
+	};
+
+})();
