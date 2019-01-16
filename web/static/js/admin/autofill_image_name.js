a24b3128fad5abb9c301d3c129a967f12e612833
Allen Garvey <allen.garvey@gmail.com>
2019-01-16 10:59:39 -0500

    Refactor JS for autofilling image name to ES6

--- web/static/js/admin/autofill_image_name.js
+++ web/static/js/admin/autofill_image_name.js
@@ -3,56 +3,61 @@
  * by using image title
  */
 (function($){
-	var imageNameSource = $('[data-image-name]');
+	const imageNameSource = $('[data-image-name]');
 	//sanity check first
 	if(imageNameSource.length < 1){
 		return;
 	}
-	var isPortraitCheckbox = document.getElementById('is_portrait_checkbox');
+
+	const isPortraitCheckbox = document.getElementById('is_portrait_checkbox');
+	const FILE_SUFFIXES = ['lg', 'med', 'sm', 'thumb'];
+	const FILE_SUFFIXES_PORTRAIT = ['med', 'med', 'sm', 'thumb'];
+
+	const TEXTFIELD_TARGETS = ['large', 'medium', 'small', 'thumbnail'].map(function(elem){
+		return document.querySelector('[data-image-' + elem + ']');
+	});
+	
 	//sets a textfield element value to 
 	//passed in value
 	//if it does not already have a value already
-	var autofillTextfield = function(textfield, value){
+	function autofillTextfield(textfield, value){
 		//val() is null if unset
-		var val = textfield.value;
+		const val = textfield.value;
 		if(val){
 			return;
 		}
 		textfield.value = value;
-	};
-	var FILE_SUFFIXES = ['lg', 'med', 'sm', 'thumb'];
-	var FILE_SUFFIXES_PORTRAIT = ['med', 'med', 'sm', 'thumb'];
-
-	var TEXTFIELD_TARGETS = ['large', 'medium', 'small', 'thumbnail'].map(function(elem){
-		return document.querySelector('[data-image-' + elem + ']');
-	});
-
-	var suffixToImageFilename = function(basename, suffix){
+	}
+	function suffixToImageFilename(basename, suffix){
 		return basename + '-' + suffix + '.jpg';
-	};
+	}
 
 	//converts string to lowercase and replaces all spaces and underscores with hyphens
-	var urlify = function(text){
+	function urlify(text){
 		if(!text){
 			return '';
 		}
 		return text.toLowerCase().replace(/&/g, 'and').replace(/@/g, 'at').replace(/[\.]+/g, ' ').replace(/[^a-z0-9-_\s]/g, '').replace(/[^\w\d]+/g, '-').replace(/^-+|-+$/g, '');
-	};
-	//when focus lost on image name source, autofill targets with pre-generated image name,
-	//unless they are already filled
-	imageNameSource.on('blur', function(event){
-		var inputText = this.value;
+	}
+
+	function autofillFields(inputText){
 		//don't do anything if input is blank
 		if(!inputText){
 			return;
 		}
-		var imageName = urlify(inputText);
-		var fileSuffixes = isPortraitCheckbox.checked ? FILE_SUFFIXES_PORTRAIT : FILE_SUFFIXES;
+		const imageName = urlify(inputText);
+		const fileSuffixes = isPortraitCheckbox.checked ? FILE_SUFFIXES_PORTRAIT : FILE_SUFFIXES;
 
 		TEXTFIELD_TARGETS.forEach(function(textfield, i){
 			autofillTextfield(textfield, suffixToImageFilename(imageName, fileSuffixes[i]));
 		});
-		
+	}
+
+
+	//when focus lost on image name source, autofill targets with pre-generated image name,
+	//unless they are already filled
+	imageNameSource.on('blur', function(event){
+		autofillFields(this.value);
 	});
 
 })(aQuery);
