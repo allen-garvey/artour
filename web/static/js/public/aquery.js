4d1d4656091a4eb05b6da0f1806703b898ef6d71
Allen Garvey <allen.garvey@gmail.com>
2019-01-21 01:09:22 -0500

    Fix indentation

--- web/static/js/public/aquery.js
+++ web/static/js/public/aquery.js
@@ -39,54 +39,54 @@ export function aQuery(selector){
 		return ret;
 	}
 
-aQueryObject.prototype.data = function(key){
-	if(this.length < 1){
-		return '';
+	aQueryObject.prototype.data = function(key){
+		if(this.length < 1){
+			return '';
+		}
+		return this.elementList[0].getAttribute('data-' + key);
 	}
-	return this.elementList[0].getAttribute('data-' + key);
-}
 
 
 
-/*
-* CSS Class methods
-*/
-aQueryObject.prototype.addClass = function(className){
-	this.each(function(i, element){
-		element.classList.add(className);
-	});
-	return this;
-};
-aQueryObject.prototype.removeClass = function(className){
-	this.each(function(i, element){
-		element.classList.remove(className);
-	});
-	return this;
-};
-/*
-* Event methods
-*/
+	/*
+	* CSS Class methods
+	*/
+	aQueryObject.prototype.addClass = function(className){
+		this.each(function(i, element){
+			element.classList.add(className);
+		});
+		return this;
+	};
+	aQueryObject.prototype.removeClass = function(className){
+		this.each(function(i, element){
+			element.classList.remove(className);
+		});
+		return this;
+	};
+	/*
+	* Event methods
+	*/
 
-aQueryObject.prototype.on = function(eventName, selector, callback){
-	//check if using delegation
-	if(selector instanceof Function){
-		//not using delegation
-		var callbackFunc = function(e){ selector.call(e.target, e); };
-	}
-	else{
-		//using delegation
-		//based on: https://davidwalsh.name/event-delegate
-		var callbackFunc = function(e){
-			if(e.target && e.target.matches(selector)){
-				callback.call(e.target, e);
-			}
-		};
-	}
-	//add event listers
-	this.each(function(i, element){
-		//element.addEventListener(eventName, function(e){callbackFunc.call(element, e);}, false);
-		element.addEventListener(eventName, callbackFunc, false);
-	});
+	aQueryObject.prototype.on = function(eventName, selector, callback){
+		//check if using delegation
+		if(selector instanceof Function){
+			//not using delegation
+			var callbackFunc = function(e){ selector.call(e.target, e); };
+		}
+		else{
+			//using delegation
+			//based on: https://davidwalsh.name/event-delegate
+			var callbackFunc = function(e){
+				if(e.target && e.target.matches(selector)){
+					callback.call(e.target, e);
+				}
+			};
+		}
+		//add event listers
+		this.each(function(i, element){
+			//element.addEventListener(eventName, function(e){callbackFunc.call(element, e);}, false);
+			element.addEventListener(eventName, callbackFunc, false);
+		});
 
-};
+	};
 }
