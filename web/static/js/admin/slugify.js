4b39a5cb2df14466fedcfadb4afbe55f03e6e864
Allen Garvey <allen.garvey@gmail.com>
2019-01-22 17:57:09 -0500

    Have regenerate image name button also regenerate image slug

--- web/static/js/admin/slugify.js
+++ web/static/js/admin/slugify.js
@@ -4,26 +4,44 @@
 
 import { aQuery as $ } from './aquery.js';
 
-export function initializeAutofillSlug(){
-	//converts string into string that can be used for slug
-	//RULES: limited to 80 characters long, only lowercase letters,
-	//numbers and hypens allowed, hyphens cannot repeat and must not
-	//start or end with hyphen
-	function slugify(text){
-		if(!text){
-			return '';
-		}
-		const slugMaxChar = 80;
-		return text.toLowerCase().replace(/[\s-_]+/g, '-').replace(/@+/g, 'at').replace(/&+/g, 'and').replace(/^[-]+|[-]$|[^a-z0-9-]/g, '').slice(0, slugMaxChar);
+//converts string into string that can be used for slug
+//RULES: limited to 80 characters long, only lowercase letters,
+//numbers and hypens allowed, hyphens cannot repeat and must not
+//start or end with hyphen
+function slugify(text){
+	if(!text){
+		return '';
 	}
+	const slugMaxChar = 80;
+	return text.toLowerCase().replace(/[\s-_]+/g, '-').replace(/@+/g, 'at').replace(/&+/g, 'and').replace(/^[-]+|[-]$|[^a-z0-9-]/g, '').slice(0, slugMaxChar);
+}
 
+function autofillSlug(target, slug, shouldForce=false){
+	if(target.value && !shouldForce){
+		return;
+	}
+	target.value = slug;
+}
+
+
+
+
+export function initializeAutofillSlug(){
 	$('[data-slug-source]').on('blur', function(event){
 		const slugSource = this.value;
 		const slugDest = document.querySelector('[data-slug]');
-    	//only replace destination text with generated slug if it doesn't have a value
-    	if(slugDest.value){
-    		return;
-    	}
-    	slugDest.value = slugify(slugSource);
+
+    	autofillSlug(slugDest, slugify(slugSource));
+	});
+
+	//initialize regenerate button
+	$('#autofill-image-button').on('click', function(event){
+		//use query selector, because there is a bug in this version of aQuery in getting value
+		const slugSource = document.querySelector('[data-slug-source]');
+		const slugDest = document.querySelector('[data-slug]');
+		if(!slugSource || !slugDest){
+			return;
+		}
+		autofillSlug(slugDest, slugify(slugSource.value), true);
 	});
 }
