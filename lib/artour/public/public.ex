c2b9d626e67154ab98cd57f1430ff16bc6a9b245
Allen Garvey <allen.garvey@gmail.com>
2019-02-07 10:12:14 -0500

    Refactor query to get a public post to reduce preloading

--- lib/artour/public/public.ex
+++ lib/artour/public/public.ex
@@ -8,6 +8,7 @@ defmodule Artour.Public do
 
   alias Artour.Post
   alias Artour.Tag
+  alias Artour.Category
 
   @doc """
   Returns the list of posts.
@@ -24,9 +25,14 @@ defmodule Artour.Public do
   Raises `Ecto.NoResultsError` if the Post does not exist.
   """
   def get_post_by_slug!(slug) do
-    from(p in Post, where: p.slug == ^slug and p.is_published == true)
+    from(
+          p in Post, 
+          join: category in assoc(p, :category), 
+          join: cover_image in assoc(p, :cover_image),   
+          where: p.slug == ^slug and p.is_published == true, 
+          preload: [category: category, cover_image: cover_image]
+        )
     |> Repo.one!
-    |> Repo.preload([:category, :cover_image])
     |> Repo.preload(tags: from(Tag, order_by: :name))
   end
 
