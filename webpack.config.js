af458fd10416a9d65239f4d91e8d85fe9911b538
Allen Garvey <allen.garvey@gmail.com>
2019-03-30 22:49:25 -0400

    Remove unnecessary webpack minification plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,8 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
-  },
   entry: {
       admin: './web/static/js/admin/admin.js',
       app: './web/static/js/public/app.js',
