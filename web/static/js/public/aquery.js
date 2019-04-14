68b67ac99afac1956046d71e5366d69a41592e99
Allen Garvey <allengarvey@hotmail.com>
2019-04-14 08:56:39 -0400

    Remove aQuery on method for public js files, since it is unused

--- web/static/js/public/aquery.js
+++ web/static/js/public/aquery.js
@@ -59,31 +59,6 @@ export function aQuery(selector){
 		});
 		return this;
 	};
-	/*
-	* Event methods
-	*/
-
-	aQueryObject.prototype.on = function(eventName, selector, callback){
-		//check if using delegation
-		if(selector instanceof Function){
-			//not using delegation
-			var callbackFunc = function(e){ selector.call(e.target, e); };
-		}
-		else{
-			//using delegation
-			//based on: https://davidwalsh.name/event-delegate
-			var callbackFunc = function(e){
-				if(e.target && e.target.matches(selector)){
-					callback.call(e.target, e);
-				}
-			};
-		}
-		//add event listers
-		this.each(function(i, element){
-			//element.addEventListener(eventName, function(e){callbackFunc.call(element, e);}, false);
-			element.addEventListener(eventName, callbackFunc, false);
-		});
-	};
 
 	return (new aQueryObject(selector));
 }
