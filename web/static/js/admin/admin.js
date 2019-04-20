d3614ec18db9b0c2084e660db1626b8b5e9b4429
Allen Garvey <allengarvey@hotmail.com>
2019-04-19 22:18:14 -0400

    Refactor getting admin vue props to use a loop

--- web/static/js/admin/admin.js
+++ web/static/js/admin/admin.js
@@ -17,12 +17,13 @@ import PostAddImagesComponent from './vues/post_add_images_component.vue';
 (function(){
     const postAddImagesContainer = document.getElementById('post_add_images_component');
     if(postAddImagesContainer){
-        const csrfToken = postAddImagesContainer.dataset.csrfToken;
-        const formUrl = postAddImagesContainer.dataset.formUrl;
-        const imagesApiUrl = postAddImagesContainer.dataset.imagesApiUrl;
+        const props = {};
+        ['csrfToken', 'formUrl', 'imagesApiUrl'].forEach((key)=>{
+            props[key] = postAddImagesContainer.dataset[key];
+        });
         new Vue({
             el: postAddImagesContainer,
-            render: h => h(PostAddImagesComponent, {props: {csrfToken, formUrl, imagesApiUrl}}),
+            render: h => h(PostAddImagesComponent, {props}),
         });
     }
 })();
@@ -31,14 +32,14 @@ import PostAlbumListComponent from './vues/post_album_list_component.vue';
 (function(){
     const postAlbumListContainer = document.getElementById('post_album_list_component');
     if(postAlbumListContainer){
-        const csrfToken = postAlbumListContainer.dataset.csrfToken;
-        const coverImageId = parseInt(postAlbumListContainer.dataset.coverImageId);
-        const postImagesApiUrl = postAlbumListContainer.dataset.postImagesApiUrl;
-        const editPostApiUrl = postAlbumListContainer.dataset.editPostApiUrl;
-        const reorderImagesApiUrl = postAlbumListContainer.dataset.reorderImagesApiUrl;
+        const props = {};
+        ['csrfToken', 'coverImageId', 'postImagesApiUrl', 'editPostApiUrl', 'reorderImagesApiUrl'].forEach((key)=>{
+            props[key] = postAlbumListContainer.dataset[key];
+        });
+
         new Vue({
             el: postAlbumListContainer,
-            render: h => h(PostAlbumListComponent, {props: {csrfToken, coverImageId, postImagesApiUrl, editPostApiUrl, reorderImagesApiUrl}}),
+            render: h => h(PostAlbumListComponent, {props}),
         });
     }
 })();
