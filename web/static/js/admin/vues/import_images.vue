06bc54b9fab000b8b321bb55b5b6f4d870480421
Allen Garvey <allengarvey@hotmail.com>
2019-04-24 22:43:53 -0400

    Add initial handlers for dragging image files into file import box

--- web/static/js/admin/vues/import_images.vue
+++ web/static/js/admin/vues/import_images.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="import-images-container">
-        <input type="file" multiple ref="fileInput" id="file_input"/>
-        <label for="file_input">
+        <input type="file" multiple ref="fileInput" id="file_input" @change="filesSelected($event)"/>
+        <label for="file_input" @drop.prevent="filesDropped($event)" @dragover.prevent="doNothing()">
             <div>
                 <!-- from: https://material.io/tools/icons/?icon=cloud_upload&style=baseline -->
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19.35 10.04C18.67 6.59 15.64 4 12 4 9.11 4 6.6 5.64 5.35 8.04 2.34 8.36 0 10.91 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96zM14 13v4h-4v-4H7l5-5 5 5h-3z"/></svg>
@@ -32,6 +32,19 @@ export default {
     watch: {
     },
     methods: {
+        doNothing(){
+
+        },
+        filesSelected(event){
+            console.log(this.$refs.fileInput.files);
+        },
+        filesDropped(event){
+            console.log(event);
+            //no foreach method for files array
+            for(let file of event.dataTransfer.files){
+                console.log(file);
+            }
+        }
     }
 };
 </script>
