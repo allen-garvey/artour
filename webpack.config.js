613b908e7b475e2802aee746f040d11d807ba564
Allen Garvey <allengarvey@hotmail.com>
2019-04-25 21:51:58 -0400

    Fix webpack errors by compiling admin styles separately

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   entry: {
       admin: './web/static/js/admin/admin.js',
       app: './web/static/js/public/app.js',
+      'admin-styles': './web/static/css/admin.scss',
   },
   output: {
     filename: '[name].min.js',
