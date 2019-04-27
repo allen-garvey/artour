f3c02b2f4d4dfddc40b0495e9f2c512bf8ea006c
Allen Garvey <allengarvey@hotmail.com>
2019-04-26 21:44:04 -0400

    Add title and slug when extracting images from files

--- web/static/js/admin/import_images.js
+++ web/static/js/admin/import_images.js
@@ -1,3 +1,5 @@
+import { titleizeStringArray } from './string.js';
+
 export function extractImages(files){
     const imageFilesMap = new Map();
 
@@ -8,11 +10,14 @@ export function extractImages(files){
             const nameWithoutExtension = name.replace(/\.\w{3,4}$/, '');
             const extension = name.replace(nameWithoutExtension, '');
             const nameDashSplit = nameWithoutExtension.split('-');
+            const title = titleizeStringArray(nameDashSplit.slice(0, nameDashSplit.length-1));
             const sizeSuffix = nameDashSplit[nameDashSplit.length - 1];
             const uniqueName = nameDashSplit.slice(0, nameDashSplit.length-1).join('-');
 
             const fileObject = imageFilesMap.has(uniqueName) ? imageFilesMap.get(uniqueName) : {};
             fileObject[sizeSuffix] = name;
+            fileObject.slug = uniqueName;
+            fileObject.title = title;
             if(!fileObject.thumb){
                 fileObject.thumb = `${uniqueName}-thumb${extension}`;
             }
