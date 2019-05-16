62fae77a1299b42a19dfd605bb29b9e844ebd483
Allen Garvey <allengarvey@hotmail.com>
2019-05-15 23:36:33 -0400

    Add a modal when viewing nsfw post for user to confirm they want to see nsfw content

--- web/controllers/public_post_controller.ex
+++ web/controllers/public_post_controller.ex
@@ -8,7 +8,7 @@ defmodule Artour.PublicPostController do
   """
   def index(conn, _params) do
     posts = Public.list_posts()
-    render conn, "index.html", page_title: "Posts", posts: posts 
+    render conn, "index.html", page_title: "Posts", posts: posts
   end
 
   @doc """
@@ -16,8 +16,8 @@ defmodule Artour.PublicPostController do
   """
   def show(conn, %{"slug" => slug}) do
     post = Public.get_post_by_slug!(slug)
-    render conn, "show.html", page_title: Artour.PostView.display_name(post), post: post, javascript: true, facebook_image: post.cover_image 
+    render conn, "show.html", page_title: Artour.PostView.display_name(post), post: post, is_nsfw: post.is_nsfw, javascript: true, facebook_image: post.cover_image
   end
 
-  
+
 end
